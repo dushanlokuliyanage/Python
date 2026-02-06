2deab8b36d8b6b1dbb4b8b5b38cf00544f884f1e
dushan loku liyanage <malingadushan27@gmail.com>
2026-02-06 06:31:46 +0530

    Conditions

---
+++ condition.py
@@ -0,0 +1,10 @@
+marks = 50
+if marks < 40:
+    print("Fail")
+    print("Garde:F")
+elif marks < 55:
+    print("Odinary Pass")
+    print("Garde:C")
+elif marks < 75:
+    print("Very Good")
+    print("Garde: A")
