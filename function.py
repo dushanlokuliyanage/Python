3138aff7d6ff3428629568a38fd04349bf2fbad5
dushan loku liyanage <malingadushan27@gmail.com>
2026-02-24 12:34:36 +0530

    Function 3

--- function.py
+++ function.py
@@ -7,3 +7,38 @@ def calTotal(price : int , quntity: int, discount):
 
 total = calTotal(500 , 5, 20)
 print(total)
+
+
+a = 1
+def f(x):
+    b = 2
+    return b + x
+
+y = f(a)
+print(y) 
+
+
+def function():
+    print("Hello")
+    function()
+
+function()
+
+def fibonachi(n):
+    if n == 0:
+        return 0
+    elif n == 1 :
+        return 1
+    else:
+        return fibonachi(n - 1) + fibonachi ( n- 2)
+
+result = fibonachi(5)
+print(result)
+
+# Local Scope
+def total(n1, n2):
+    tot = n1 + n2
+    return tot
+result = total(10,20)
+print(result)
+
